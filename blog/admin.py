bea56ff1e5fc5b4a236ebc6c9d844305119ee94b
Administrator <admin@example.com>
2021-03-29 17:52:53 +0300

    fixed admin pages load time

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields=['likes', 'tags']
+
 admin.site.register(Tag)
-admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    raw_id_fields = ['post', 'author']
+    list_select_related = ['post', 'author']
