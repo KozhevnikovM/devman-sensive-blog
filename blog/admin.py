174ff46decf7c8464cf17f54b47bbe6b00ca6284
Administrator <admin@example.com>
2021-03-29 19:23:54 +0300

    format with pylint

--- blog/admin.py
+++ blog/admin.py
@@ -4,11 +4,13 @@ from blog.models import Post, Tag, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """Post admin view"""
     raw_id_fields=['likes', 'tags']
 
 admin.site.register(Tag)
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """Comment admin view"""
     raw_id_fields = ['post', 'author']
     list_select_related = ['post', 'author']
